4d541261fee27e9e58bd8ab577b12be362f70673
tbdevnet <tbdevnet@0ba3a8e8-f30d-0410-9ac9-feb71648d401>
2007-12-01 22:41:19 +0000

    Rearranged some code functions moved functions in to include files

--- include/bittorrent.php
+++ include/bittorrent.php
@@ -23,12 +23,12 @@ $maxusers = 75000; // LoL Who we kiddin' here?
 $maxusers = 5000;
 
 // ONLY USE ONE OF THE FOLLOWING DEPENDING ON YOUR O/S!!!
-$torrent_dir = "/var/tb/torrents";    # FOR UNIX ONLY - must be writable for httpd user
-//$torrent_dir = "C:/web/Apache2/htdocs/tbsource/torrents";    # FOR WINDOWS ONLY - must be writable for httpd user
+//$torrent_dir = "/var/tb/torrents";    # FOR UNIX ONLY - must be writable for httpd user
+$torrent_dir = "F:/web/xampp/htdocs/tb/torrents";    # FOR WINDOWS ONLY - must be writable for httpd user
 
 # the first one will be displayed on the pages
 $announce_urls = array();
-$announce_urls[] = "http://localhost/TB/announce.php";
+$announce_urls[] = "http://localhost/TBDEV/announce.php";
 $announce_urls[] = "http://domain.com:82/announce.php";
 $announce_urls[] = "http://domain.com:83/announce.php";
 
@@ -514,84 +514,7 @@ function deletetorrent($id) {
     unlink("$torrent_dir/$id.torrent");
 }
 
-function pager($rpp, $count, $href, $opts = array()) {
-    $pages = ceil($count / $rpp);
 
-    if (!$opts["lastpagedefault"])
-        $pagedefault = 0;
-    else {
-        $pagedefault = floor(($count - 1) / $rpp);
-        if ($pagedefault < 0)
-            $pagedefault = 0;
-    }
-
-    if (isset($_GET["page"])) {
-        $page = 0 + $_GET["page"];
-        if ($page < 0)
-            $page = $pagedefault;
-    }
-    else
-        $page = $pagedefault;
-
-    $pager = "";
-
-    $mp = $pages - 1;
-    $as = "<b>&lt;&lt;&nbsp;Prev</b>";
-    if ($page >= 1) {
-        $pager .= "<a href=\"{$href}page=" . ($page - 1) . "\">";
-        $pager .= $as;
-        $pager .= "</a>";
-    }
-    else
-        $pager .= $as;
-    $pager .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-    $as = "<b>Next&nbsp;&gt;&gt;</b>";
-    if ($page < $mp && $mp >= 0) {
-        $pager .= "<a href=\"{$href}page=" . ($page + 1) . "\">";
-        $pager .= $as;
-        $pager .= "</a>";
-    }
-    else
-        $pager .= $as;
-
-    if ($count) {
-        $pagerarr = array();
-        $dotted = 0;
-        $dotspace = 3;
-        $dotend = $pages - $dotspace;
-        $curdotend = $page - $dotspace;
-        $curdotstart = $page + $dotspace;
-        for ($i = 0; $i < $pages; $i++) {
-            if (($i >= $dotspace && $i <= $curdotend) || ($i >= $curdotstart && $i < $dotend)) {
-                if (!$dotted)
-                    $pagerarr[] = "...";
-                $dotted = 1;
-                continue;
-            }
-            $dotted = 0;
-            $start = $i * $rpp + 1;
-            $end = $start + $rpp - 1;
-            if ($end > $count)
-                $end = $count;
-            $text = "$start&nbsp;-&nbsp;$end";
-            if ($i != $page)
-                $pagerarr[] = "<a href=\"{$href}page=$i\"><b>$text</b></a>";
-            else
-                $pagerarr[] = "<b>$text</b>";
-        }
-        $pagerstr = join(" | ", $pagerarr);
-        $pagertop = "<p align=\"center\">$pager<br />$pagerstr</p>\n";
-        $pagerbottom = "<p align=\"center\">$pagerstr<br />$pager</p>\n";
-    }
-    else {
-        $pagertop = "<p align=\"center\">$pager</p>\n";
-        $pagerbottom = $pagertop;
-    }
-
-    $start = $page * $rpp;
-
-    return array($pagertop, $pagerbottom, "LIMIT $start,$rpp");
-}
 
 function downloaderdata($res) {
     $rows = array();
