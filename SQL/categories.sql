09670b0b915cc2dccb8ebbc81b0b22cae8c99926
tbdevnet <tbdevnet@0ba3a8e8-f30d-0410-9ac9-feb71648d401>
2008-01-05 13:09:32 +0000

    data structures

---
+++ SQL/categories.sql
@@ -0,0 +1,29 @@
+-- phpMyAdmin SQL Dump
+-- version 2.9.2
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Jan 05, 2008 at 01:02 PM
+-- Server version: 5.0.33
+-- PHP Version: 5.2.1
+-- 
+-- Database: `tb`
+-- 
+
+-- 
+-- Dumping data for table `categories`
+-- 
+
+INSERT INTO `categories` VALUES (1, 'Appz/PC ISO', 'cat_apps.gif');
+INSERT INTO `categories` VALUES (4, 'Games/PC ISO', 'cat_games.gif');
+INSERT INTO `categories` VALUES (5, 'Movies/SVCD', 'cat_movies.gif');
+INSERT INTO `categories` VALUES (6, 'Music', 'cat_music.gif');
+INSERT INTO `categories` VALUES (7, 'Episodes', 'cat_episodes.gif');
+INSERT INTO `categories` VALUES (9, 'XXX', 'cat_xxx.gif');
+INSERT INTO `categories` VALUES (12, 'Games/GBA', 'cat_games.gif');
+INSERT INTO `categories` VALUES (17, 'Games/PS2', 'cat_games.gif');
+INSERT INTO `categories` VALUES (23, 'Anime', 'cat_anime.gif');
+INSERT INTO `categories` VALUES (19, 'Movies/XviD', 'cat_movies.gif');
+INSERT INTO `categories` VALUES (20, 'Movies/DVD-R', 'cat_movies.gif');
+INSERT INTO `categories` VALUES (21, 'Games/PC Rips', 'cat_games.gif');
+INSERT INTO `categories` VALUES (22, 'Appz/misc', 'cat_apps.gif');
