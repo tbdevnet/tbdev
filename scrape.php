2faad7a8be078b50bffc967f5523d2cae2e9b0af
tbdevnet <tbdevnet@0ba3a8e8-f30d-0410-9ac9-feb71648d401>
2010-03-19 16:40:13 +0000

    19th March 2010 Scrape update added single scrape and min interval flags

--- scrape.php
+++ scrape.php
@@ -8,49 +8,56 @@
 |   http://www.tbdev.net
 |   =============================================
 |   svn: http://sourceforge.net/projects/tbdevnet/
-|   Licence Info: GPL
+|   Licence Info: Creative Commons 2
 +------------------------------------------------
-|   $Date$
-|   $Revision$
-|   $Author$
-|   $URL$
+|   $Date: 2009-09-05 18:48:06 +0100 (Sat, 05 Sep 2009) $
+|   $Revision: 199 $
+|   $Author: tbdevnet $
+|   $URL: https://tbdevnet.svn.sourceforge.net/svnroot/tbdevnet/trunk/TB/scrape.php $
 +------------------------------------------------
 */
 require_once("include/config.php");
 
-if (!@mysql_connect($TBDEV['mysql_host'], $TBDEV['mysql_user'], $TBDEV['mysql_pass']))
-  {
-	  exit();
-  }
+    if (!@mysql_connect($TBDEV['mysql_host'], $TBDEV['mysql_user'], $TBDEV['mysql_pass']))
+    {
+      exit();
+    }
+      
+    @mysql_select_db($TBDEV['mysql_db']) or exit();
+
+    if ( !isset($_GET['info_hash']) OR (strlen($_GET['info_hash']) != 20) )
+      error('Invalid hash');
+
+    $res = @mysql_query( "SELECT info_hash, seeders, leechers, times_completed FROM torrents WHERE " . hash_where( $_GET['info_hash']) );
     
-  @mysql_select_db($TBDEV['mysql_db']) or exit();
+    if( mysql_num_rows($res) )
+      error('No torrent with that hash found');
+    
+    $benc = 'd5:files';
 
-function hash_where($name, $hash) 
-  {
-    $shhash = preg_replace('/ *$/s', "", $hash);
-    return "$name = '" . mysql_real_escape_string($hash) . "'";
-  }
+    while ($row = mysql_fetch_assoc($res))
+    {
+      $benc .= 'd20:'.pack('H*', $row['info_hash'])."d8:completei{$row['seeders']}e10:downloadedi{$row['times_completed']}e10:incompletei{$row['leechers']}eee";
+    }
+
+    $benc .= 'ed5:flagsd20:min_request_intervali1800eee';
+
+    header('Content-Type: text/plain; charset=UTF-8');
+    header('Pragma: no-cache');
+    print($benc);
 
 
-  $r = 'd5:filesd';
+function error($err){
 
-  $fields = "info_hash, times_completed, seeders, leechers";
+    header('Content-Type: text/plain; charset=UTF-8');
+    header('Pragma: no-cache');
+    exit("d14:failure reason".strlen($err).":{$err}ed5:flagsd20:min_request_intervali1800eeee");
+    
+}
 
-  if (!isset($_GET["info_hash"]))
-    $query = "SELECT $fields FROM torrents ORDER BY info_hash";
-  else
-    $query = "SELECT $fields FROM torrents WHERE " . hash_where( "info_hash", @pack('H*', stripslashes( $_GET["info_hash"] ) ));
+function hash_where($hash) {
 
-  $res = mysql_query($query);
+    return "info_hash = '" . mysql_real_escape_string( bin2hex($hash) ) . "'";
 
-  while ($row = mysql_fetch_assoc($res))
-  {
-    $r .= '20:'.str_pad($row['info_hash'], 20).'d8:completei'.$row['seeders'].'e10:downloadedi'.$row['times_completed'].'e10:incompletei'.$row['leechers'].'ee';
-  }
-
-  $r .= 'ee';
-
-  header("Content-Type: text/plain");
-  print($r);
-
+}
 ?>
